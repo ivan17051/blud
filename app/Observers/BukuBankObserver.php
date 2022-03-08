059d1709388727ce9c48e71bbb5d74d8fbe0cada
Ivan Indrakusuma <ivan_indrakusuma@yahoo.com>
2022-03-08 15:44:21 +0700

    Loop update saldo

--- app/Observers/BukuBankObserver.php
+++ app/Observers/BukuBankObserver.php
@@ -15,10 +15,12 @@ class BukuBankObserver
         DB::beginTransaction();
         try {
             $old = BukuBank::find($bukubank->id);
+            // jika create baru
             if(!$old){
                 $old=(object) array('jenis'=>0, 'nominal'=>0);
                 $this->updateSaldo($bukubank->idunitkerja , $bukubank->jenis, $old->jenis, $bukubank->tanggal, $bukubank->nominal, $old->nominal);
             }
+            // jika dihapus
             elseif($bukubank->isactive==0){
                 $this->updateSaldo($bukubank->idunitkerja , $bukubank->jenis, $old->jenis, $bukubank->tanggal, 0, $old->nominal);
             }
@@ -34,6 +36,8 @@ class BukuBankObserver
     private function updateSaldo($idunitkerja, $jenis_new, $jenis_old, $tanggal, $saldo_new, $saldo_old){
         $tanggal = substr($tanggal,0,8)."01";
         $tanggal2 = Carbon::make($tanggal);
+        $tanggal_now = Carbon::now()->translatedFormat('Y-m-')."01";
+        
         $saldo=SaldoBukuBank::where('idunitkerja', $idunitkerja)->whereYear('tanggal',$tanggal2->year)->whereMonth('tanggal',$tanggal2->month)->first();
         
         if($saldo === NULL){
@@ -70,8 +74,32 @@ class BukuBankObserver
             $saldo->nominal+=$saldo_old;
             $saldo->nominal-=$saldo_new;
         }
-
+        
         $saldo->save();
+        if($tanggal_now>$tanggal){
+            $tanggal = Carbon::make($tanggal);
+            $tanggal_now = Carbon::make($tanggal_now);
+            for($x=$tanggal->month+1;$x<=$tanggal_now->month;$x++){
+                $saldo=SaldoBukuBank::where('idunitkerja', $idunitkerja)->whereYear('tanggal', $tanggal->year)->whereMonth('tanggal', $x)->first();
+                if($jenis_new==1 && $jenis_old==1){
+                    $saldo->nominal-=$saldo_old;
+                    $saldo->nominal+=$saldo_new;
+                }
+                elseif($jenis_new==1 && $jenis_old==0){
+                    $saldo->nominal+=$saldo_old;
+                    $saldo->nominal+=$saldo_new;
+                }
+                elseif($jenis_new==0 && $jenis_old==1){
+                    $saldo->nominal-=$saldo_old;
+                    $saldo->nominal-=$saldo_new;
+                }
+                elseif($jenis_new==0 && $jenis_old==0){
+                    $saldo->nominal+=$saldo_old;
+                    $saldo->nominal-=$saldo_new;
+                }
+                $saldo->save();
+            }
+        }
     }
 
 }
